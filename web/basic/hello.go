6b26e63762a70ba9b57cb392fc1541615680dc87
Samir Gadkari <samir.gadkari@gmail.com>
2021-10-15 10:10:03 -0700

    Added server with timeouts and graceful shutdown

--- web/basic/hello.go
+++ web/basic/hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/samirgadkari/learn/web/basic/handlers"
 )
@@ -18,5 +21,28 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sig_chan := make(chan os.Signal)
+	signal.Notify(sig_chan, os.Interrupt)
+	signal.Notify(sig_chan, os.Kill)
+
+	sig := <-sig_chan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
